fdcc2130d037ad8db42e27758d4499608af92810
rendipriam <56166846+rendipriam@users.noreply.github.com>
2019-10-05 00:35:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,16 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
+import task
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template('index.html')
+    name = request.args.get('name', 'John doe')
+    result = task.hello.delay(name)
+    result.wait()
+    return render_template('index.html', celery=result)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
